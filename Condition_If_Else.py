34e885a27e86234bfa84c4b55856b95a820a149a
wabq120 <wabq120@gmail.com>
2019-01-24 10:55:03 +0100

    remove somes others folders

--- Condition_If_Else.py
+++
@@ -1,8 +0,0 @@
-Number=input("enter number:")
-if (int(Number)>0):
-    print("Number is positive")
-else:
-    print("number is negative")
-
-print("App is done") #pour voir passage de debug
-# si on fait if , if , else ( le else va etre l'opposé du if qui le precede seulement !)
--- Condition_Simple.py
+++
@@ -1,9 +0,0 @@
-import datetime
-DOB=input("Enter your DOB:")
-CurrentYear=datetime.datetime.now().year
-Age=CurrentYear-int(DOB)
-if(Age>=18):
-    print("Your age is {} and you are Adult".format(Age))
-if(Age<18):
-    print("Your age is {} and you are not Adult".format(Age))
-print("App is done")
