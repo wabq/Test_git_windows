ac44707f21f99948080f1b0d0153e8078e2b1fdd
wabq120 <wabq120@gmail.com>
2019-01-24 10:49:17 +0100

    remove somes folders

--- Exceptions.py
+++
@@ -1,8 +0,0 @@
-
-def main():
-    try:
-        x=int( input("enter number"))
-        print(x+5)
-    except ValueError:
-        print(" value have to be interger")
-if __name__ == '__main__':main()
